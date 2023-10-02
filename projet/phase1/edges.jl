24b3ced0be33fdd3ef34f63cee4ae4cc1be9dd84
Antoine Demers-Bergeron <antoine.demers-bergeron@polymtl.ca>
2023-10-02 12:11:21 -0400

    Phase 1 - Autres changements

---
+++ projet/phase1/edges.jl
@@ -0,0 +1,35 @@
+import Base.show
+
+
+"""Type abstrait dont d'autres types d'arêtes dériveront."""
+abstract type AbstractEdge{T} end
+
+"""Type représentant les arêtes d'un graphe.
+
+Exemple:
+
+        edge = Edge(poids, node1, node2)
+        
+
+"""
+mutable struct Edge{T,Y} <: AbstractEdge{T}
+  weight::T
+  node1::Node{Y}
+  node2::Node{Y}
+end
+
+# on présume que toutes les arêtes dérivées de AbstractEdge
+# posséderont des champs 'weight', 'node1' et 'node2'.
+
+"""Renvoie le poids de l'arête."""
+weight(edge::AbstractEdge) = edge.weight
+
+"""Renvoie les deux noeuds de l'arête"""
+nodes(edge::AbstractEdge) = edge.node1, edge.node2
+
+
+
+"""Affiche un noeud."""
+function show(edge::AbstractEdge)
+  println("Arête de ", name(nodes(edge)[1]), " à ", name(nodes(edge)[2]), ", poids: ", weight(edge))
+end
--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -1,7 +1,8 @@
-include("graph.jl")
-include("edges.jl")
-include("node.jl")
 
+include("node.jl")
+include("edges.jl")
+include("graph.jl")
+include("read_stsp.jl")
 
 node1 = Node("1",3)
 node2 = Node("2",3)
@@ -9,3 +10,11 @@ node2 = Node("2",3)
 edge = Edge(3,node1,node2)
 
 show(edge)
+
+graphe = Graph("Graphe1", Node{Int}[], Edge{Int,Int}[])
+
+add_edge!(graphe,edge)
+
+show(graphe)
+
+read_stsp("instances\\stsp\\gr17.tsp")
