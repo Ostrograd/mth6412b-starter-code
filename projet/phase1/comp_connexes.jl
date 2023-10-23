c7073ed61babbc1d3fe2107cefa2ced658e04a42
Antoine Demers-Bergeron <antoine.demers-bergeron@polymtl.ca>
2023-10-23 11:58:45 -0400

    Ajout de Structure de données pour les composantes connexes et les arbres

---
+++ projet/phase1/comp_connexes.jl
@@ -0,0 +1,96 @@
+include("node.jl")
+include("edges.jl")
+include("graph.jl")
+import Base.show
+
+
+"""Type abstrait dont d'autres types de graphes dériveront."""
+abstract type AbstractGraph{T} end
+
+"""Type representant un graphe comme un ensemble de noeuds.
+
+Exemple :
+
+    node1 = Node("Joe", 3.14)
+    node2 = Node("Steve", exp(1))
+    node3 = Node("Jill", 4.12)
+    G = Graph("Ick", [node1, node2, node3])
+
+Attention, tous les noeuds doivent avoir des données de même type.
+"""
+mutable struct Comp_Connexe{Y,T} <: AbstractGraph{T}
+  name::String
+  nodes::Vector{Node{Y}}
+  edges::Vector{Edge{T,Y}}
+end
+
+"""Ajoute un noeud au graphe."""
+function add_node!(graph::Comp_Connexe{Y,T}, node::Node{Y}) where {Y,T}
+  push!(graph.nodes, node)
+  graph
+end
+
+"""Ajoute une arête au graphe"""
+function add_edge!(graph::Comp_Connexe{Y,T}, edge::Edge{T,Y}) where {Y,T}
+  push!(graph.edges, edge)
+  if !(edge.node1.name in nodes_names(graph))
+    add_node!(graph, edge.node1)
+    @warn "Le noeud",edge.node1.name," n'était pas dans le graphe. Il a été ajouté."
+  end
+  if !(edge.node2.name in nodes_names(graph))
+    add_node!(graph, edge.node2)
+    @warn "Le noeud",edge.node2.name," n'était pas dans le graphe. Il a été ajouté."
+  end
+  graph
+end
+
+
+
+"""Affiche un graphe"""
+function show(graph::Comp_Connexe)
+  println("Graph ", name(graph), " has ", nb_nodes(graph), " nodes.")
+  for node in nodes(graph)
+    show(node)
+  end
+  println("Graph ", name(graph), " has ", nb_edges(graph), " edges.")
+  for edge in edges(graph)
+    show(edge)
+  end
+end
+
+
+######################### Arbre : #############################
+
+
+mutable struct Tree{Y,T} <: AbstractGraph{T}
+    name::String
+    nodes::Vector{Node{Y}}
+    edges::Vector{Edge{T,Y}}
+    root::Node{Y}
+end
+
+"""Renvoie la racine de la composante connexe"""
+root(graph::Tree) = graph.root
+
+"""Ajoute un noeud au graphe."""
+function add_node!(graph::Tree{Y,T}, node::Node{Y}) where {Y,T}
+  push!(graph.nodes, node)
+  graph
+end
+
+
+"""Ajoute une arête au graphe"""
+function add_edge!(graph::Tree{Y,T}, edge::Edge{T,Y}) where {Y,T}
+  push!(graph.edges, edge)
+  if !(edge.node1.name in nodes_names(graph))
+    add_node!(graph, edge.node1)
+    @warn "Le noeud",edge.node1.name," n'était pas dans le graphe. Il a été ajouté."
+  end
+  if !(edge.node2.name in nodes_names(graph))
+    add_node!(graph, edge.node2)
+    @warn "Le noeud",edge.node2.name," n'était pas dans le graphe. Il a été ajouté."
+  end
+  graph
+end
+##To do : Vérifier que add_edge! ne crée pas de cycle.
+##          Ajouter les hauteurs pour les nodes
